d846f4dd6e4bc5f6983a40d1f5566dd3911f1699
Brandon Apol <brandonapol@cedarville.edu>
2022-09-23 12:31:30 -0800

    displaying weather

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,6 +1,90 @@
 <template>
-    <div>
+    <div class="flex flex-col flex-1 items-center">
+        <div
+        v-if="route.query.preview"
+        class="text-white p-4 bg-weather-secondary w-full text-center"
+        >
+            <p>
+            You are currently previewing this city, click the "+"
+            icon to start tracking this city.
+            </p>
+        </div>
+        <div class="flex flex-col items-center text-white py-12">
+            <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+            <p class="text-sm mb-12">
+            {{
+            new Date(weatherData.currentTime).toLocaleDateString(
+                "en-us",
+                {
+                weekday: "short",
+                day: "2-digit",
+                month: "long",
+                }
+            )
+            }}
+            {{
+            new Date(weatherData.currentTime).toLocaleTimeString(
+                "en-us",
+                {
+                timeStyle: "short",
+                }
+            )
+            }}
+            </p>
+        </div>
+        <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
+      <p>
+        Feels like
+        {{ Math.round(weatherData.current.feels_like) }} &deg;
+      </p>
+      <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img
+        class="w-[150px] h-auto"
+        :src="
+          `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+        "
+        alt=""
+      />
+    </div>
 
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Hourly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+        <div class="mx-8 text-white">
+            <h2 class="mb-4">Hourly Weather</h2>
+            <div class="flex gap-10 overflow-x-scroll">
+                <div
+                    v-for="hourData in weatherData.hourly"
+                    :key="hourData.dt"
+                    class="flex flex-col gap-4 items-center"
+                >
+                    <p class="whitespace-nowrap text-md">
+                        {{
+                            new Date(
+                                hourData.currentTime
+                                ).toLocaleTimeString("en-us", {
+                                hour: "numeric",
+                            })
+                        }}
+                    </p>
+                    <img
+                        class="w-auto h-[50px] object-cover"
+                        :src="
+                            `http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`
+                        "
+                        alt=""
+                    />
+                    <p class="text-xl">
+                        {{ Math.round(hourData.temp) }}&deg;
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +93,8 @@ import axios from "axios";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+console.log(route)
+console.log('This is the route ^ \n\n\n')
 const getWeatherData = async () => {
     try {
         const weatherData = await axios.get(
