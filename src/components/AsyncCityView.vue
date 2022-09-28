a317bea021f446334702429121951d2c92f1019a
Brandon Apol <brandonapol@cedarville.edu>
2022-09-28 15:48:59 -0700

    fully functioning app

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,55 +1,56 @@
 <template>
     <div class="flex flex-col flex-1 items-center">
-        <div
+
+      <div
         v-if="route.query.preview"
         class="text-white p-4 bg-weather-secondary w-full text-center"
-        >
-            <p>
-            You are currently previewing this city, click the "+"
-            icon to start tracking this city.
-            </p>
-        </div>
-        <div class="flex flex-col items-center text-white py-12">
-            <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
-            <p class="text-sm mb-12">
-            {{
+      >
+        <p>
+          You are currently previewing this city, click the "+"
+          icon to start tracking this city.
+        </p>
+      </div>
+
+      <div class="flex flex-col items-center text-white py-12">
+        <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+        <p class="text-sm mb-12">
+          {{
             new Date(weatherData.currentTime).toLocaleDateString(
-                "en-us",
-                {
+              "en-us",
+              {
                 weekday: "short",
                 day: "2-digit",
                 month: "long",
-                }
+              }
             )
-            }}
-            {{
+          }}
+          {{
             new Date(weatherData.currentTime).toLocaleTimeString(
-                "en-us",
-                {
+              "en-us",
+              {
                 timeStyle: "short",
-                }
+              }
             )
-            }}
-            </p>
-        </div>
+          }}
+        </p>
         <p class="text-8xl mb-8">
-        {{ Math.round(weatherData.current.temp) }}&deg;
-      </p>
-      <p>
-        Feels like
-        {{ Math.round(weatherData.current.feels_like) }} &deg;
-      </p>
-      <p class="capitalize">
-        {{ weatherData.current.weather[0].description }}
-      </p>
-      <img
-        class="w-[150px] h-auto"
-        :src="
-          `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
-        "
-        alt=""
-      />
-    </div>
+          {{ Math.round(weatherData.current.temp) }}&deg;
+        </p>
+        <p>
+          Feels like
+          {{ Math.round(weatherData.current.feels_like) }} &deg;
+        </p>
+        <p class="capitalize">
+          {{ weatherData.current.weather[0].description }}
+        </p>
+        <img
+          class="w-[150px] h-auto"
+          :src="
+            `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+          "
+          alt=""
+        />
+      </div>
 
     <hr class="border-white border-opacity-10 border w-full" />
 
@@ -90,42 +91,51 @@
     <hr class="border-white border-opacity-10 border w-full" />
 
     <div class="max-w-screen-md w-full py-12">
-        <div class="mx-8 text-white">
-            <h2 class="mb-4">7 Day Forecast</h2>
-            <div
-                v-for="day in weatherData.daily"
-                :key="day.dt"
-                class="flex items-center"
-            >
-                <p class="flex-1">
-                    {{
-                    new Date(day.dt * 1000).toLocaleDateString(
-                        "en-us",
-                        {
-                        weekday: "long",
-                        }
-                    )
-                    }}
-                </p>
-                <img
-                    class="w-[50px] h-[50px] object-cover"
-                    :src="
-                    `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
-                    "
-                    alt=""
-                />
-                <div class="flex gap-2 flex-1 justify-end">
-                    <p>H: {{ Math.round(day.temp.max) }}</p>
-                    <p>L: {{ Math.round(day.temp.min) }}</p>
-                </div>
-            </div>
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 Day Forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString(
+                "en-us",
+                {
+                  weekday: "long",
+                }
+              )
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="
+              `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
+            "
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}</p>
+            <p>L: {{ Math.round(day.temp.min) }}</p>
+          </div>
         </div>
+      </div>
     </div>
+
+    <div
+      class="flex items-center gap-2 py-12 text-white cursor-pointer duration-150 hover:text-red-500"
+      @click="removeCity"
+    >
+      <i class="fa-solid fa-trash"></i>
+      <p>Remove City</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const getWeatherData = async () => {
@@ -154,5 +164,22 @@ const getWeatherData = async () => {
 };
 
 const weatherData = await getWeatherData();
+
+const router = useRouter();
+const removeCity = () => {
+    const cities = JSON.parse(localStorage.getItem("savedCities"));
+    console.log(cities)
+    const updatedCities = cities.filter(
+        (city) => city.id !== route.query.id 
+    );
+    console.log(updatedCities);
+    localStorage.setItem(
+        "savedCities",
+        JSON.stringify(updatedCities)
+    );
+    router.push({
+        name: "home",
+    });
+};
 </script>
 
